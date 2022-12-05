f3b54832607b70a5cbef76ac6b0bfe663bbdc0dc
TretyakovKonstantin <spb_unex@mail.ru>
2022-12-05 16:30:03 +0300

    Решена первая задача

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String exampleString = "1234";
+        System.out.println("Это строка - " + exampleString + ", полидром?, " + thisStringPolydrom(exampleString));
+    }
+
+    public static boolean thisStringPolydrom(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }

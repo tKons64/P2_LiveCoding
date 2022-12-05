0a4fb5b38f74e731ebd60115dd487a5c5a71476a
TretyakovKonstantin <spb_unex@mail.ru>
2022-12-05 16:35:26 +0300

    Решена вторая задача

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        //задача №1
         String exampleString = "1234";
-        System.out.println("Это строка - " + exampleString + ", полидром?, " + thisStringPolydrom(exampleString));
+        System.out.println("Это строка - *" + exampleString + "*, полидром?, " + thisStringPolydrom(exampleString));
+
+        //задача №2
+        exampleString = "123 456 7890";
+        System.out.println("Исходная строка - *" + exampleString + "*, и эта строка без пробелов - *" + delSpace(exampleString) + "*");
+
     }
 
     public static boolean thisStringPolydrom(String s) {
@@ -12,5 +18,15 @@ public class Main {
             }
         }
         return true;
+    }
+
+    public static String delSpace(String s) {
+        String newString = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                newString = newString + s.charAt(i);
+            }
+        }
+        return newString;
     }
 }
